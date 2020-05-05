38f343c9fc08551f0dc712bb694aebe06dd91eca
angsabrina <sabrina.ang@gatech.edu>
2020-05-04 21:56:54 -0400

    copied penguinArea code from tutorial

--- Assets/Penguin/Scripts/PenguinAcademy.cs
+++ Assets/Penguin/Scripts/PenguinAcademy.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using MLAgents;
 
-public class PenguinAcademy : MonoBehaviour
+public class PenguinAcademy : Academy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// Gets/sets the current fish speed
+    /// </summary>
+    public float FishSpeed { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Gets/sets the current acceptable feed radius
+    /// </summary>
+    public float FeedRadius { get; private set; }
+
+    /// <summary>
+    /// Called when the academy first gets initialized
+    /// </summary>
+    public override void InitializeAcademy()
     {
-        
+        FishSpeed = 0f;
+        FeedRadius = 0f;
+
+        // Set up code to be called every time the fish_speed parameter changes 
+        // during curriculum learning
+        FloatProperties.RegisterCallback("fish_speed", f =>
+        {
+            FishSpeed = f;
+        });
+
+        // Set up code to be called every time the feed_radius parameter changes 
+        // during curriculum learning
+        FloatProperties.RegisterCallback("feed_radius", f =>
+        {
+            FeedRadius = f;
+        });
     }
 }
